a3c21698366e03cd593bf8719ab703f87c646a47
NAPAS KAVALEE <napas.kavalee@gmail.com>
2020-01-13 19:41:39 +0700

    --

---
+++ src/components/StoreInfo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class StoreInfo extends Component {
+    render() {
+        return (
+            <div>
+              <p>StoreInfo</p>
+            </div>
+        )
+    }
+}
+
+export default StoreInfo
