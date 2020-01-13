6c00ed4fe0246772b773b87bef95b1d18a782304
underdofg <38998878+underdofg@user.norely.github.com>
2020-01-13 19:43:18 +0700

    test

---
+++ src/components/StoreReviews.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class StoreReviews extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default StoreReviews
