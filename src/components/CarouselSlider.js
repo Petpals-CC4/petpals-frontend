069fc923be9d59aae97e807d8ae20b88b0d8487c
YoRHa54i <pol25852@hotmail.com>
2020-01-14 16:01:01 +0700

    14Jan20 carousel slider complect

--- src/components/CarouselSlider.js
+++ src/components/CarouselSlider.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react'
+import { Row, Col, Carousel, Layout, Card } from 'antd'
 
 export class CarouselSlider extends Component {
+  state = {
+    data: {
+      image_url: [
+        "https://img.designswan.com/2018/11/JPGroom/1.jpg",
+        "https://img.designswan.com/2018/11/JPGroom/2.jpg",
+        "https://img.designswan.com/2018/11/JPGroom/3.jpg",
+        "https://img.designswan.com/2018/11/JPGroom/4.jpg"
+      ]
+    }
+  }
   render() {
     return (
-      <div>
-        
-      </div>
+      <Layout>
+        <Card>
+          <Row className="row-carousel">
+            <Col >
+              <Carousel autoplay>
+                {
+                  this.state.data.image_url.map(url => (
+                    <div className="slide-pet" >
+                      <img src={url} style={{ maxWidth: "100%", display: "block", margin: "auto" }}
+                      />
+                    </div>
+                  ))
+                }
+              </Carousel>
+            </Col>
+          </Row>
+        </Card>
+      </Layout>
+
     )
   }
 }
