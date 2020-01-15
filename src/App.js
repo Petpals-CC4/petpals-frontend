896cccc92dbc0b46dc9715c0b16d04098018de9d
Chinnawat Chimdee <chinnawat.cpre@gmail.com>
2020-01-15 16:19:45 +0700

    [Edited] - convert function to class

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.css';
-//import LandingPage from './components/LandingPage/LandingPage';
+// import LandingPage from './components/LandingPage/LandingPage';
 // import Navbar from './components/landing/Navbar'
 // import StoreInfo from "./components/store-detail/StoreInfo";
 // import PaymentPage from "./pages/PaymentPage";
 
+class App extends Component {
+  render() {
+    return (
+      <div>
 
-function App() {
-  return (
-    <div>
-        {/* <Navbar></Navbar> */}
-      {/* <StoreInfo /> */}
-      {/* <PaymentPage /> */}
-      {/* <VerifyPaymentPage /> */}
-      {/* <CarouselSlider /> */}
-    </div>
-  );
+        {/* <Navbar /> */}
+        {/* <StoreInfo /> */}
+        {/* <PaymentPage /> */}
+        {/* <VerifyPaymentPage /> */}
+        {/* <CarouselSlider /> */}
+      </div>
+    )
+  }
 }
 
 export default App
