5974c334776d0b796915278da93b743094bd96a8
NAPAS KAVALEE <napas.kavalee@gmail.com>
2020-01-15 15:30:02 +0700

    [Not finish] - หลอกหลอน component

---
+++ src/components/store-detail/Affix.js
@@ -0,0 +1,9 @@
+<Affix
+offsetTop={120}
+onChange={affixed => console.log(affixed)}
+style={{ position: "absolute", left: "50%" }}
+>
+<Card>
+  <Button type="primary">จอง</Button>
+</Card>
+</Affix>
