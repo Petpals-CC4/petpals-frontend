89f2233538ac5e1f8e53a79d6aec0dff944c87d7
NAPAS KAVALEE <napas.kavalee@gmail.com>
2020-01-15 17:53:17 +0700

    [Added] - StoreDetailPage.js and AffixServicePrice.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 //import LandingPage from './components/LandingPage/LandingPage';
-import Navbar from './components/LandingPage/Navbar'
+import Navbar from "./components/LandingPage/Navbar";
 import StoreInfo from "./components/store-detail/StoreInfo";
 import PaymentPage from "./pages/PaymentPage";
-
+import AffixServicePrice from "./components/store-detail/AffixServicePrice";
+import StoreDetailPage from "./pages/StoreDetailPage";
 
 function App() {
   return (
     <div>
-        {/* <Navbar></Navbar> */}
-      {/* <StoreInfo /> */}
+      {/* <Navbar></Navbar> */}
+      <StoreDetailPage />
       {/* <PaymentPage /> */}
       {/* <VerifyPaymentPage /> */}
-      {/* <CarouselSlider /> */}
     </div>
   );
 }
 
-export default App
+export default App;
