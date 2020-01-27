20268ec6a18b4d3d2079b6d28223e976677fc943
underdofg <38998878+underdofg@user.norely.github.com>
2020-01-27 19:34:12 +0700

    Merge branch 'feature/film_DashBoard&&EdditSotreBio' into develop
    # Conflicts:
    #	src/pages/ShopDetailEditPage.js

--- src/pages/ShopDetailEditPage.js
+++ src/pages/ShopDetailEditPage.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
-=======
-import React, { Component } from 'react'
-import { Layout } from 'antd';
-import { Row,Col } from 'antd';
-import { Button } from 'antd';
-import EditStoreBio from '../components/store-bio-edit/EditStoreBio';
->>>>>>> feature/film_DashBoard&&EdditSotreBio
 
-
+import EditStoreBio from "../components/store-bio-edit/EditStoreBio"
 import EditStoreService from "../components/store-service-edit/EditStoreService";
 import StoreBank from "../components/store-detail-edit/StoreBank";
 import FooterZone from "../components/landing/FooterZone";
@@ -17,23 +9,14 @@ import StoreAddress from "../components/store-detail-edit/StoreAddress";
 class ShopDetailEditPage extends Component {
   render() {
     return (
-<<<<<<< HEAD
       <>
+        <EditStoreBio />
         <StoreAddress />
         <EditStoreService />
         <StoreBank />
         <FooterZone />
       </>
     );
-=======
-      <div >
-          <Layout className="fullMinHeight">
-            <EditStoreBio />
-            <EditStoreService />
-      </Layout>
-      </div>
-    )
->>>>>>> feature/film_DashBoard&&EdditSotreBio
   }
 }
 
