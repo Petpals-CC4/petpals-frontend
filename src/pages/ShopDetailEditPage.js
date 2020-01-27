74d5528f3a7f7b09412fc3331ce2f56161cb7191
NAPAS KAVALEE <napas.kavalee@gmail.com>
2020-01-27 23:21:48 +0700

    [Added] StoreAddress component editing complete

--- src/pages/ShopDetailEditPage.js
+++ src/pages/ShopDetailEditPage.js
@@ -4,7 +4,7 @@ import React, { Component } from "react";
 import EditStoreService from "../components/store-service-edit/EditStoreService";
 import StoreBank from "../components/store-detail-edit/StoreBank";
 import FooterZone from "../components/landing/FooterZone";
-import StoreAddress from "../components/store-detail-edit/StoreAddress";
+import StoreAddress from "../../src/components/store-detail-edit/store-adress-edit/StoreAddress";
 
 class ShopDetailEditPage extends Component {
   render() {
