aeae0d94d6fe01e759c1708b88494132f5812911
underdofg <38998878+underdofg@user.norely.github.com>
2020-01-30 15:50:10 +0700

    no message

--- src/components/DashboardNanny/OrderProgess/OrderProgress.js
+++ src/components/DashboardNanny/OrderProgess/OrderProgress.js
@@ -1,17 +1,42 @@
 import React, { Component } from 'react'
-import { Steps } from 'antd';
+import { Steps, Icon } from 'antd';
 
-const Step = Steps.Step;
+const { Step } = Steps;
+
+const stepStyle = {
+    marginBottom: 60,
+    boxShadow: '0px -1px 0 0 #e8e8e8 inset',
+  };
 
 export class OrderProgress extends Component {
+    
+    state = {
+        current: 0,
+      };
+
+    onChange = current => {
+        console.log('onChange:', current);
+        this.setState({ current });
+      };
+
     render() {
+     const { current } = this.state;
         return (
             <div>
-                <Steps current={1}>
-                <Step title="Finished" description="This is a description." />
-                <Step title="In Progress" description="This is a description." />
-                <Step title="Waiting" description="This is a description." />
-                </Steps>
+                <Steps
+                    type="navigation"
+                    size="small"
+                    current={current}
+                    onChange={this.onChange}
+                    style={stepStyle}
+                >
+            </Steps>
+                <Steps type="navigation" current={current} onChange={this.onChange} style={stepStyle}>
+                <Step status="finish" title="รอการชำระเงิน" />
+                <Step status="process" title="รอร้านค้ายืนยัน"  />
+                <Step status="wait" title="ออเดอร์สำเร็จ" />
+                <Step status="wait" title="Step 4" />
+            </Steps>
             </div>
         )
     }
